d87d54dea823d08d5387831ddb6443ca3ba8cc24
unknown <53538316+grlucy@users.noreply.github.com>
2020-03-31 14:53:51 -0400

    added phone number to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <div className="row">
           <div className="col-md-12 text-white text-center text-md-left">
             <p className="footerPara mb-0 d-flex align-items-center justify-content-center justify-content-md-start">
-              &copy; Gina Lucy 2020
+              &copy; 2020 Gina Lucy • (757) 652-3703
             </p>
           </div>
         </div>
