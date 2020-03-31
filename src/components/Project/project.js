56fc9c7a2e64ebe4542b27b91e5cf89227a5b33e
unknown <53538316+grlucy@users.noreply.github.com>
2020-03-31 16:22:08 -0400

    created projects component map

---
+++ src/components/Project/project.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Project(props) {
+  return (
+    <div className="col-md-4">
+      <div className="projectLink" onClick={props.onClick(props.projectObj)}>
+        <div className="card">
+          <img
+            className="card-img-top staticGif"
+            src={props.staticImg}
+            alt={props.imgAlt}
+          />
+          <img
+            className="card-img-top hoverGif"
+            src={props.demoGif}
+            alt={props.imgAlt}
+          />
+          <div className="card-body">
+            <p className="card-title text-center">{props.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Project from "../../components/Project/project";
+
+import ProjectsList from "../../projects.json";
 
 import "./Projects.css";
 
 function Projects() {
-  return <h1>Projects</h1>;
+  const [showProject, setShowProject] = useState();
+
+  const projectClick = projectObj => {
+    console.log(projectObj);
+    console.log("hi");
+  };
+
+  return (
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center text-md-left">
+            <h3 className="sectionTitle">#projects</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 text-center text-md-left">
+            <h5>
+              <i>Click to expand:</i>
+            </h5>
+          </div>
+        </div>
+        <div className="row">
+          {ProjectsList.map(project => (
+            <Project
+              key={project.name}
+              projectObj={project}
+              staticImg={project.staticImg}
+              imgAlt={project.imgAlt}
+              demoGif={project.demoGif}
+              name={project.name}
+              onClick={projectClick}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
